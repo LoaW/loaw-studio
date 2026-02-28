50262ff415c1a4f30fe2d8b56559b795341dcef6
Xavier Brigode <xavier.brigode@gmail.com>
2026-02-28 11:09:54 +0100

    initial structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Header } from './sections/header/header';
+import { Hero } from './sections/hero/hero';
+import { Showcase } from './sections/showcase/showcase';
+import { Process } from './sections/process/process';
+import { Contact } from './sections/contact/contact';
+import { Footer } from './sections/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Hero, Showcase, Process, Contact, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
